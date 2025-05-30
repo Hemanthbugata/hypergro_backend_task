f8033a177077176da77ae1ffa68874c5302c29c2
hemanthbugata <hemanthbugata@gmail.com>
2025-05-30 17:10:32 +0530

    code

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,10 @@ app.use('/property', propertyRoutes);
 app.use('/favorites', favoritesRoutes);
 app.use('/recommendations', recommendationsRouter);
 
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
