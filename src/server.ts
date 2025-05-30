cc77b2732cefdb017538208a8765a0d5029016ae
hemanthbugata <hemanthbugata@gmail.com>
2025-05-30 17:24:39 +0530

    code

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI || '', { })
+mongoose.connect(process.env.MONGODB_URI || '', { })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -30,3 +30,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app; 
